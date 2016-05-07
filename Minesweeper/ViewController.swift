04bab76f78f5a82dacb13cca036d8b666d6ff232
John Rizkalla <johnrizkalla@hotmail.com>
2016-05-07 10:04:57 -0400

    Initial Commit

---
+++ Minesweeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by John Rizkalla on 2016-05-07.
+//  Copyright © 2016 John Rizkalla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
